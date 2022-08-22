fd5938009e9e49d064f8437b5a5151620d5083b9
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-22 15:20:50 +0200

    add html for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Layout from "./components/Layout/Layout";
-
+import Counter from "./components/Counter/Counter";
 const App = (): JSX.Element => {
-  return <Layout />;
+  return (
+    <>
+      <Layout />
+      <Counter />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,24 @@
+const Counter = (): JSX.Element => {
+  return (
+    <ul>
+      <li>
+        <div>02</div>
+        <h2>days</h2>
+      </li>
+      <li>
+        <div>20</div>
+        <h2>hours</h2>
+      </li>
+      <li>
+        <div>34</div>
+        <h2>minutes</h2>
+      </li>
+      <li>
+        <div>02</div>
+        <span>seconds</span>
+      </li>
+    </ul>
+  );
+};
+
+export default Counter;
