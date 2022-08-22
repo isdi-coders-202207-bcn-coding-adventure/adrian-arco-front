dead4e580f937bddcdc9998f2ca01a369cd14c58
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-22 15:27:56 +0200

    changed counter to layour component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import Layout from "./components/Layout/Layout";
-import Counter from "./components/Counter/Counter";
 const App = (): JSX.Element => {
-  return (
-    <>
-      <Layout />
-      <Counter />
-    </>
-  );
+  return <Layout />;
 };
 
 export default App;
