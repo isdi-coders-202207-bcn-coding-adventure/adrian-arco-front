6c5e8650bdc1a5737a03563e6adb21f91801e73d
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-22 16:01:20 +0200

    add styled component CounterStyled

---
+++ src/components/Counter/Counter.Styled.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const CounterStyled = styled.ul`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  gap: 20px;
+  background-color: #243342;
+  height: 300px;
+  font-family: "Oxanium";
+
+  & .list-item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &__timer {
+      font-size: 2.5rem;
+      display: flex;
+      color: #243342;
+      justify-content: center;
+      align-items: center;
+      width: 90px;
+      height: 90px;
+      background-color: #ffc700;
+      border-radius: 20px;
+    }
+    &__text {
+      color: white;
+    }
+  }
+`;
+
+export default CounterStyled;
